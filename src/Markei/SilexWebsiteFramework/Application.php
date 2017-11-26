dda5fd4b6847c3339996c795304f467d877f96ae
Maarten de Keizer <github@maartendekeizer.nl>
2017-11-26 17:54:02 +0100

    Don't read Twig configuration from application config but configure via code

--- src/Markei/SilexWebsiteFramework/Application.php
+++ src/Markei/SilexWebsiteFramework/Application.php
@@ -69,9 +69,6 @@ class Application extends \Silex\Application
                 'smtp.password',
                 'smtp.encryption',
                 'smtp.auth_mode',
-                'twig.path',
-                'twig.form.templates',
-                'twig.options',
                 'form.secret',
                 'translation.locale'
             ];
@@ -97,11 +94,7 @@ class Application extends \Silex\Application
      */
     protected function bootTwig()
     {
-        $options = array(
-            'twig.path' => is_array($this['twig.path']) ? $this['twig.path'] : [$this['twig.path']],
-            'twig.form.templates' => $this['twig.form.templates'],
-            'twig.options' => $this['twig.options']
-        );
+        $options = $this->getTwigOptions();
         $options['twig.path'][] = __DIR__ . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views';
         if ($this['debug'] === false) {
             $options['twig.options']['cache'] = $this['app.cache'] . DIRECTORY_SEPARATOR . 'twig';
@@ -147,6 +140,19 @@ class Application extends \Silex\Application
                         'auth_mode' => $this['smtp.auth_mode']
                     )
             ));
+    }
+
+    /**
+     * Get an array of options for Twig configuration
+     * @return array
+     */
+    protected function getTwigOptions()
+    {
+        return [
+            'twig.path' => [],
+            'twig.form.templates' => [],
+            'twig.options' => isset($this['twig.options']) ? $this['twig.options'] : [],
+        ];
     }
 
     /**
