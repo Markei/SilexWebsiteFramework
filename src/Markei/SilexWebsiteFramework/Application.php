5b21b275a772634b9967249ef3a56403da525260
Maarten de Keizer <github@maartendekeizer.nl>
2017-12-29 17:29:12 +0100

    Add placeholder for database integration

--- src/Markei/SilexWebsiteFramework/Application.php
+++ src/Markei/SilexWebsiteFramework/Application.php
@@ -86,6 +86,7 @@ class Application extends \Silex\Application
     {
         parent::boot();
         $this->bootTwig();
+        $this->bootDatabase();
         $this->bootForm();
         $this->bootTranslation();
         $this->bootSwiftmailer();
@@ -154,6 +155,15 @@ class Application extends \Silex\Application
                         'auth_mode' => $this['smtp.auth_mode']
                     )
             ));
+    }
+
+    /**
+     * Boots the Database Service Provider
+     * It's empty by default, please override with your own implementation
+     */
+    protected function bootDatabase()
+    {
+        //
     }
 
     /**
